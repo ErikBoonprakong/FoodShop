66b60efe51fb236008b8539731d231d5d7aa0610
ErikBoonprakong <e.boonprakong@gmail.com>
2024-01-18 19:58:17 +0000

    Added checkout form server-side validation

--- FoodShop/Controllers/OrderController.cs
+++ FoodShop/Controllers/OrderController.cs
@@ -14,8 +14,34 @@ namespace FoodShop.Controllers
             _shoppingCart = shoppingCart;
         }
 
-        public IActionResult Checkout()
+        public IActionResult Checkout() //GET
         {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Checkout(Order order)
+        {
+            var items = _shoppingCart.GetShoppingCartItems();
+            _shoppingCart.ShoppingCartItems = items;
+
+            if (_shoppingCart.ShoppingCartItems.Count == 0)
+            {
+                ModelState.AddModelError("", "Your cart is empty, add some pies first");
+            }
+
+            if (ModelState.IsValid)
+            {
+                _orderRepository.CreateOrder(order);
+                _shoppingCart.ClearCart();
+                return RedirectToAction("CheckoutComplete");
+            }
+            return View(order);
+        }
+
+        public IActionResult CheckoutComplete()
+        {
+            ViewBag.CheckoutCompleteMessage = "Thanks for your order. You'll soon enjoy our delicious pies!";
             return View();
         }
     }
